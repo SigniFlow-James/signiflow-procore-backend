deb436d6855bd8305620f520ddd588b6c882c318
James <johnny.signiflow@gmail.com>
2026-01-12 08:26:16 +0200

    remove unneccesary formatting option for base64 conversion

--- Services/SigniflowService.cs
+++ Services/SigniflowService.cs
@@ -49,7 +49,7 @@ public class SigniflowService
             }
 
             // ---------------- DOCUMENT ----------------
-            var encodedPdf = Convert.ToBase64String(pdfBytes, Base64FormattingOptions.InsertLineBreaks);
+            var encodedPdf = Convert.ToBase64String(pdfBytes);
             Console.WriteLine($"📄 PDF size: {pdfBytes.Length} bytes, encoded size: {encodedPdf.Length} chars");
 
             var workflowRequest = new FullWorkflowRequest
